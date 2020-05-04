38dad05f8bc2c3f0a1561481e69593f1fa5e7e72
ben135wang <61523434+ben135wang@users.noreply.github.com>
2020-05-03 21:32:31 -0400

    Initial sound commit

---
+++ Assets/Scripts/SoundMixer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMixer : MonoBehaviour
+{
+
+    public static AudioSource bgm;
+    public static AudioSource soundeffect;
+
+    public AudioClip track;
+
+    public AudioClip test;
+
+    public static readonly Dictionary<string, AudioClip> sounds = new Dictionary<string, AudioClip>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        bgm = this.gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        soundeffect = this.gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+
+        bgm.loop = true;
+        bgm.clip = track;
+        bgm.volume = 0.5f;
+        bgm.Play();
+
+        sounds.Add("test sound effect", test);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    /*    public static void PlaySound(AudioSource source, string sound, float volume = 1.0f)
+        {
+            source.PlayOneShot(sounds[sound], volume);
+        }*/
+
+}
+
